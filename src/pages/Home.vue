f6a15f060e8ef1b380ed4aa6eaf8efb9b82ae5e8
Siddhesh Mangela <sidthecool007@gmail.com>
2021-07-01 11:28:43 +0530

    paths added

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">Home</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  setup: () => {
+    console.log('yo');
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
