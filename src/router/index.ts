76c8a72e2e67b32f1c5bc099620f19d7bfed1cab
Siddhesh Mangela <sidthecool007@gmail.com>
2021-07-07 12:11:55 +0530

    fix lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,3 @@
-import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
@@ -10,7 +9,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/:id',
     name: 'Details',
-    component: defineAsyncComponent(() => import('pages/Details.vue')),
+    component: () => import('pages/Details.vue'),
   },
 ];
 
